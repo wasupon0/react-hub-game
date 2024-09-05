5c4fbf0ee4afa3952aefc471ee180ba4f5d67128
wasupon <wasupon.mail@gmail.com>
2024-09-05 14:17:39 +0900

    feat: add hook platform selector

---
+++ src/hooks/usePlatforms.ts
@@ -0,0 +1,10 @@
+import useData from "./useData";
+
+interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+export default function usePlatforms() {
+  return useData<Platform[]>("/platforms/lists/parents");
+}
