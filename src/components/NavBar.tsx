9e4dbb4db9a5d9401648cb45423bfd0f3842be88
wasupon <wasupon.mail@gmail.com>
2024-09-05 15:16:41 +0900

    feat: search game logic

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,17 @@
 import { HStack, Image } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 import logo from "/public/logo.svg";
 
-export default function NavBar() {
+interface Props {
+  onSearch: (searchTerm: string) => void;
+}
+
+export default function NavBar({ onSearch }: Props) {
   return (
     <HStack justifyContent={"space-between"} padding="10px">
       <Image src={logo} boxSize={"60px"} />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
