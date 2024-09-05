747559a005eb8cf032fdff39810bc50e5c983f00
wasupon <wasupon.mail@gmail.com>
2024-09-05 12:42:18 +0900

    feat: add game platform icons to each card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Game, Platform } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
 
 export interface Props {
   game: Game;
@@ -11,6 +12,13 @@ export default function GameCard({ game }: Props) {
       <Image src={game.background_image} alt={game.name} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        {game.parent_platforms.map(({ platform }: Platform) => (
+          // <Text key={platform.id}>{platform.name}</Text>
+          <></>
+        ))}
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,45 @@
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import type { IconType } from "react-icons";
+import { BsGlobe } from "react-icons/bs";
+import {
+  FaAndroid,
+  FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { Game, Platform } from "../hooks/useGames";
+
+interface Props {
+  platforms: Platform[];
+}
+
+export default function PlatformIconList({ platforms }: Props) {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    mac: FaApple,
+    linux: FaLinux,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon
+          boxSize={8}
+          as={iconMap[platform.slug]}
+          key={platform.id}
+          color="gray.500"
+        />
+      ))}
+    </HStack>
+  );
+}
